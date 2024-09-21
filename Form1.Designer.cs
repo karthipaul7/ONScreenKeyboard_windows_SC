075660543bbf7f97264cd1244e7b6a74fc749ce8
karthipaul7 <111708345+karthipaul7@users.noreply.github.com>
2024-09-21 20:27:45 +0530

    initial push2

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SocketApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtReceivedMessages = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtReceivedMessages
+            // 
+            this.txtReceivedMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReceivedMessages.Location = new System.Drawing.Point(30, 40);
+            this.txtReceivedMessages.Name = "txtReceivedMessages";
+            this.txtReceivedMessages.Size = new System.Drawing.Size(625, 35);
+            this.txtReceivedMessages.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(713, 40);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(281, 26);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnStartServer.Location = new System.Drawing.Point(219, 94);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(239, 54);
+            this.btnStartServer.TabIndex = 2;
+            this.btnStartServer.Text = "Connect to Server";
+            this.btnStartServer.UseVisualStyleBackColor = false;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.Location = new System.Drawing.Point(713, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button2.Location = new System.Drawing.Point(219, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 54);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 601);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtReceivedMessages);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtReceivedMessages;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace SocketApp
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener _listener;
+        private Thread _listenerThread;
+        private const int Port = 5000;
+        private bool isBroadcasting = false;
+        private HttpListener httpListener;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void StartHttpServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+            listener.Start();
+            Console.WriteLine("Listening for connections...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                // Send a message to confirm the app is running on this PC
+                string message = "App is running";
+                byte[] data = Encoding.ASCII.GetBytes(Environment.MachineName);
+                stream.Write(data, 0, data.Length);
+
+                client.Close();
+            }
+        }
+
+        private void StartServer()
+        {
+            _listener = new TcpListener(IPAddress.Any, Port);
+            _listener.Start();
+           // AppendText("Server started and waiting for a connection...");
+
+            while (true)
+            {
+                TcpClient client = _listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                AppendText(message);
+
+                // Send acknowledgment to the client
+               // string response = "Message received";
+               // byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+               // stream.Write(responseBytes, 0, responseBytes.Length);
+
+                client.Close();
+            }
+        }
+
+        private void AppendText(string text)
+        {
+            var specialCharacters = new HashSet<String> { "+", "^", "%", "~", "(", ")", "{", "}", "[", "]" };
+
+            if (specialCharacters.Contains(text))
+            {
+                SendKeys.SendWait("{" + text + "}");
+            }
+            else
+            {
+                SendKeys.SendWait(text);
+            }
+            
+        }
+
+        // Helper method to escape special characters
+        private string EscapeSpecialCharacters(string input, HashSet<char> specialChars)
+        {
+            string result = "";
+
+            foreach (char c in input)
+            {
+                if (specialChars.Contains(c))
+                {
+                    result += "{" + c + "}";
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            return result;
+        }
+
+       
+        // Function to get the local IPv4 address
+        private string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _listener?.Stop();
+            _listenerThread?.Abort();
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+             _listenerThread = new Thread(StartServer);
+             _listenerThread.IsBackground = true;
+             _listenerThread.Start();
+             btnStartServer.Enabled = false;
+            MessageBox.Show("Connected");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ip = "192.168.0.105"; // IP address of the Kotlin server
+            int port = 8080; // Port number where the Kotlin server is listening
+            string message = txtMessage.Text; // Message to send (from a TextBox)
+
+            try
+            {
+                // Create a TcpClient and connect to the server
+                using (TcpClient client = new TcpClient("192.168.0.105", 8080))
+
+                {
+                    // Convert message to bytes
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+
+                    // Get the stream for writing
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        // Write data to the stream
+                        stream.Write(data, 0, data.Length);
+                       // MessageBox.Show("Message sent to the server");
+
+                        // Optionally, read a response from the server
+                        using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                        {
+                            string response = reader.ReadLine();
+                           // MessageBox.Show("Response from server: " + response);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            _listenerThread = new Thread(StartHttpServer);
+            _listenerThread.IsBackground = true;
+            _listenerThread.Start();
+            button2.Enabled = false;
+        }
+    }
+}
